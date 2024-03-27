87b9e1adda795da4d3338f2fec9e3f1ba427e39a
albertostizzoli <albertostizzoli@yahoo.it>
2024-03-27 15:53:24 +0100

    added comments on FizzBuzz

--- src/org/experis/fizzbuzz/FizzBuzz.java
+++ src/org/experis/fizzbuzz/FizzBuzz.java
@@ -2,20 +2,29 @@ package org.experis.fizzbuzz;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-
-        for(int i = 1; i <= 100; i++){
-
-            if(i % 3 == 0 && i % 5 == 0){
+        // Ciclo da 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            // Controlla se i è multiplo sia di 3 che di 5
+            if (i % 3 == 0 && i % 5 == 0) {
+                // Se multiplo di entrambi, stampa FizzBuzz
                 System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
+            }
+            // Controlla se i è multiplo di 3
+            else if (i % 3 == 0) {
+                // Se multiplo di 3, stampa Fizz
                 System.out.println("Fizz");
-            } else if(i % 5 == 0 ){
+            }
+            // Controlla se i è multiplo di 5
+            else if (i % 5 == 0) {
+                // Se multiplo di 5, stampa Buzz
                 System.out.println("Buzz");
-            } else{
+            }
+            // Se non è multiplo di 3 o di 5, stampa il numero stesso
+            else {
                 System.out.println(i);
             }
         }
-
     }
 }
+
 
